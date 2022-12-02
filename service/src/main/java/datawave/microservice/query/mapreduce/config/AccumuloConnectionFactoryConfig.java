c6be31a1f8ab586652f6579e6de796e4a65145fd
Whitney O'Meara <james.w.omeara@gmail.com>
2022-12-02 17:02:25 -0500

    Initial commit for map reduce service

---
+++ service/src/main/java/datawave/microservice/query/mapreduce/config/AccumuloConnectionFactoryConfig.java
@@ -0,0 +1,24 @@
+package datawave.microservice.query.mapreduce.config;
+
+import datawave.core.common.cache.AccumuloTableCache;
+import datawave.core.common.connection.AccumuloConnectionFactory;
+import datawave.core.common.connection.AccumuloConnectionFactoryImpl;
+import datawave.core.common.result.ConnectionPoolsProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AccumuloConnectionFactoryConfig {
+    @Bean
+    @ConfigurationProperties("datawave.connection.factory")
+    public ConnectionPoolsProperties connectionPoolsProperties() {
+        return new ConnectionPoolsProperties();
+    }
+    
+    @Bean
+    public AccumuloConnectionFactory accumuloConnectionFactory(AccumuloTableCache accumuloTableCache, ConnectionPoolsProperties connectionPoolsProperties) {
+        return AccumuloConnectionFactoryImpl.getInstance(accumuloTableCache, connectionPoolsProperties);
+    }
+}
